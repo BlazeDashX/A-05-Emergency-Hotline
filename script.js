39ee0d4d6242876453778943bf52f874a4b108fc
BlazeDashX <refat00021@gmail.com>
2026-05-06 05:38:27 +0600

    implemented copy count tracking

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 let heartCount = parseInt(document.getElementById('heart-count').innerText);
-const coinCount  = parseInt(document.getElementById('coin-count').innerText);
-const copyCount  = parseInt(document.getElementById('copy-count').innerText);
+let coinCount  = parseInt(document.getElementById('coin-count').innerText);
+let copyCount  = parseInt(document.getElementById('copy-count').innerText);
 
 // heart increase
-
 const heartUp = document.getElementsByClassName("heart");
 
 for (let btn of heartUp) {
     btn.addEventListener("click", function () {
         heartCount++;
         document.getElementById("heart-count").innerText = heartCount;
+    });
+}
+
+// Copy increase
+const copyUp = document.getElementsByClassName("copy");
+
+for (let btn of copyUp) {
+    btn.addEventListener("click", function () {
+        copyCount++;
+        document.getElementById("copy-count").innerText = copyCount;
     });
 }
